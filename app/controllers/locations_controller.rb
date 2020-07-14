fa1e69413589c7109c8082c4e38a759fb28253db
Christian <christianchrr@gmail.com>
2020-07-14 17:41:04 -0400

    setup location controller/views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,40 @@ class LocationsController < ApplicationController
         @locations = Location.all
     end
 
+    def new
+        @location = Location.new
+    end
+
+    def create
+        @location = Location.new(location_params(:name, :address, :city, :state, :zip))
+        if @location.save
+            flash[:message] = "Location created!"
+            redirect_to location_path(@location)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @location.update(location_params(:name, :address, :city, :state, :zip))
+          redirect_to location_path(@location)
+        else
+          render :edit
+        end
+    end
+    
+    def destroy
+        @location.destroy
+        redirect_to locations_path
+    end
+
+    private
+
+    def location_params(*args)
+        params.require(:location).permit(*args)
+    end
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
     has_many :events
     has_many :users, through: :events
+
+    validates_presence_of :name, :address, :city, :state, :zip
 end
