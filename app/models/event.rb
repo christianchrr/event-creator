5db08094fdbf532800bdfc64b6680747f6de0acd
Christian <christianchrr@gmail.com>
2020-07-19 18:20:35 -0400

    adds styling

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
     belongs_to :location
     accepts_nested_attributes_for :location
 
-    scope :event_group, -> { order("name")}
+    scope :event_group, -> { order("name") }
 
     validates_presence_of :name, :description, :month, :day, :year
 end
